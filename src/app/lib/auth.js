f5ddec3be09b75e28d42e820734c870ad71ae1cc
mrashed21 <muhammadrashed760@gmail.com>
2025-01-24 15:40:30 +0600

    [UPDATE] update middleware

--- src/app/lib/auth.js
+++ src/app/lib/auth.js
@@ -1,13 +1,31 @@
-import { getKindeServerSession } from "@kinde-oss/kinde-auth-nextjs/server";
-import { redirect } from "next/navigation";
+// import { getKindeServerSession } from "@kinde-oss/kinde-auth-nextjs/server";
+// import { redirect } from "next/navigation";
 
-export async function checkAuthentication() {
+// export async function checkAuthentication() {
+//   const { isAuthenticated } = getKindeServerSession();
+//   const authenticated = await isAuthenticated();
+
+//   if (!authenticated) {
+//     redirect("/api/auth/login");
+//   }
+
+//   return authenticated;
+// }
+import { getKindeServerSession } from "@kinde-oss/kinde-auth-nextjs/server";
+import { NextResponse } from "next/server";
+
+export async function middleware(req) {
   const { isAuthenticated } = getKindeServerSession();
   const authenticated = await isAuthenticated();
 
   if (!authenticated) {
-    redirect("/api/auth/login");
+    const url = new URL("/api/auth/login", req.url);
+    return NextResponse.redirect(url);
   }
 
-  return authenticated;
+  return NextResponse.next();
 }
+
+export const config = {
+  matcher: ["/protected-route/:path*", "/another-protected-path/:path*"], // Define your protected routes
+};
